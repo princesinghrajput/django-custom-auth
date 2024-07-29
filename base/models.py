4c3e777db4fa11832d2ae1938ab07e1597840264
princesinghrajput <psr8084@gmail.com>
2024-07-29 10:56:42 +0530

    email pass based login

--- base/models.py
+++ base/models.py
@@ -5,4 +5,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True)
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS = ['']
